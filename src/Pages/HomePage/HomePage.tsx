81ff520509252decf4dfacb3a82a78df36fd1bdd
Mohammad Anees <aman6129@neo.tamu.edu>
2020-07-25 17:27:49 -0500

    add router. add material ui (#1)

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Box, Typography } from '@material-ui/core';
+
+const HomePage: React.FC = () => {
+  return (
+    <Box margin='auto' px={2}>
+      <Typography variant='h5'>Home Page</Typography>
+    </Box>
+  );
+};
+
+export default HomePage;
